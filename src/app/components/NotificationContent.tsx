f928006403d170210df7e7be6d77340b4a304ca4
NyiArrManHein <manheinn@gmail.com>
2024-09-04 16:13:23 +0630

    fixed searchParams

---
+++ src/app/components/NotificationContent.tsx
@@ -0,0 +1,32 @@
+import { useSearchParams } from "next/navigation";
+import Html from "./Html";
+
+const NotificationContent = () => {
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const content = searchParams.get("content");
+
+  if (!title || !content) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Html showNavbar={true}>
+      <div className="flex flex-row w-full p-5 h-screen">
+        <div className="w-1/4 lg:w-6/12 hidden sm:flex"></div>
+        <div className="w-full px-3">
+          <div className="card card-bordered border-base-300 flex flex-col text-justify p-3 mb-1 sm:text-sm text-base min-h-64">
+            <div className="card-title">Reported Post Preview</div>
+            <div className="card-body text-lg sm:text-2xl pb-0">{title}</div>
+            <div className="card-body text-lg whitespace-pre-line overflow-auto">
+              {content}
+            </div>
+          </div>
+        </div>
+        <div className="w-1/4 lg:w-6/12 hidden sm:flex"></div>
+      </div>
+    </Html>
+  );
+};
+
+export default NotificationContent;
