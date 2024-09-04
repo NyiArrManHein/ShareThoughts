6cfffa74b5d3adc2ce81e7599fd924eeb7f2392e
NyiArrManHein <manheinn@gmail.com>
2024-09-04 15:46:39 +0630

    fixed auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,41 +1,50 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Register from "../components/Register";
 import Login from "../components/Login";
 import { FlashMessage } from "@/lib/models";
 import { useSearchParams } from "next/navigation";
+import AuthComponent from "../components/AuthComponent";
 
 function Auth() {
   const [isRegister, setIsRegister] = useState(true);
   const [flashMessage, setFlashMessage] = useState<FlashMessage | undefined>(
     undefined
   );
-  const searchParams = useSearchParams();
-  const isLogIn = searchParams.get("isRegister") === "false";
+  // const searchParams = useSearchParams();
+  // const isLogIn = searchParams.get("isRegister") === "false";
 
-  useEffect(() => {
-    if (isLogIn) {
-      setIsRegister(false);
-    }
-  }, [isLogIn]);
+  // useEffect(() => {
+  //   if (isLogIn) {
+  //     setIsRegister(false);
+  //   }
+  // }, [isLogIn]);
 
   return (
-    <main className="flex flex-row justify-center mt-4">
-      {isRegister ? (
-        <Register
-          setIsRegister={setIsRegister}
-          flashMessage={flashMessage}
-          setFlashMessage={setFlashMessage}
-        />
-      ) : (
-        <Login
-          setIsRegister={setIsRegister}
-          flashMessage={flashMessage}
-          setFlashMessage={setFlashMessage}
-        />
-      )}
-    </main>
+    // <main className="flex flex-row justify-center mt-4">
+    //   {isRegister ? (
+    //     <Register
+    //       setIsRegister={setIsRegister}
+    //       flashMessage={flashMessage}
+    //       setFlashMessage={setFlashMessage}
+    //     />
+    //   ) : (
+    //     <Login
+    //       setIsRegister={setIsRegister}
+    //       flashMessage={flashMessage}
+    //       setFlashMessage={setFlashMessage}
+    //     />
+    //   )}
+    // </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthComponent
+        isRegister={isRegister}
+        setIsRegister={setIsRegister}
+        flashMessage={flashMessage}
+        setFlashMessage={setFlashMessage}
+      />
+    </Suspense>
   );
 }
 
